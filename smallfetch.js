a14bfd19155694e8fbe2d74c4125f74b3049708f
Alberto <alnavarro@cloud.hosts.mx>
2018-01-22 18:33:14 +0000

    Fixing states

--- smallfetch.js
+++ smallfetch.js
@@ -1,16 +1,15 @@
 const rs = 'application/json';
-export default (url,method,json,shouldFail=true) => {
-    if(typeof method !== 'string' ) return fetch(url,fetch);
-    if(typeof json === 'boolean') { 
-      shouldFail = json; 
-      json = undefined; 
-    }
-    return fetch(url,{
-        method,  
-        headers: {
-            'Accept':rs ,
-            'Content-Type': rs
-        },
-        body: JSON.stringify(json)
-    }).then(x=>{if(!x.ok&&shouldFail)throw x; return x.json()});
+export default (url, method = 'GET', json, shouldFail = (json?json:true) ) => {
+  if (typeof method !== 'string') return fetch(url, method);
+  return fetch(url, {
+    method,
+    headers: {
+      'Accept': rs,
+      'Content-Type': rs
+    },
+    body: JSON.stringify(json)
+  }).then(x => {
+    if (!x.ok && shouldFail) throw x;
+    return x.json()
+  });
 };
